0e951f93f263418a08952bd7558b33ea9fbba8af
Allegro <alex.makovka@gmail.com>
2023-04-24 12:38:35 +0300

    add arguments and output format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,10 @@ import { program } from 'commander';
 program.description(`Compares two configuration files and shows a difference.`);
 //program.option('-V, --version', 'output the version number');
 program.version('0.0.1', '-V, --version', 'output the version number');
+program.option('-f, --format <type>', 'output format');
+
+program.argument('<filepath1>'); //, 'path to first file');
+program.argument('<filepath2>'); // , 'path to second file');
 
 /* program.addHelpText(
   'after',
