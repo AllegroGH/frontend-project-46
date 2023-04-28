8a04713b2387524e17e6307951f79dbc8d9e44cc
Allegro <alex.makovka@gmail.com>
2023-04-28 13:31:10 +0300

    edit index.js

--- src/index.js
+++ src/index.js
@@ -14,34 +14,33 @@ const jsonDiff = (filepath1, filepath2) => {
   const fullPath1 = pathResolve(cwd(), filepath1);
   const fullPath2 = pathResolve(cwd(), filepath2);
 
-  if (existsSync(fullPath1) && existsSync(fullPath2)) {
-    const jsonFile1 = JSON.parse(readFileSync(fullPath1, 'utf8'));
-    const jsonFile2 = JSON.parse(readFileSync(fullPath2, 'utf8'));
-    const sortedKeys = _.sortBy(_.union(Object.keys(jsonFile1), Object.keys(jsonFile2)));
-    const firstFilePrefix = '  - ';
-    const secondFilePrefix = '  + ';
-    const bothFilesPrefix = '    ';
-    const valueDelimiter = ': ';
+  if (!existsSync(fullPath1) && !existsSync(fullPath2)) return 'Incorrect data';
 
-    const diffArray = sortedKeys.reduce(
-      (acc, key) => {
-        if (Object.hasOwn(jsonFile1, key)) {
-          if (Object.hasOwn(jsonFile2, key)) {
-            if (jsonFile1[key] === jsonFile2[key]) {
-              acc.push(bothFilesPrefix, key, valueDelimiter, jsonFile1[key], '\n');
-            } else {
-              acc.push(firstFilePrefix, key, valueDelimiter, jsonFile1[key], '\n');
-              acc.push(secondFilePrefix, key, valueDelimiter, jsonFile2[key], '\n');
-            }
-          } else acc.push(firstFilePrefix, key, valueDelimiter, jsonFile1[key], '\n');
-        } else acc.push(secondFilePrefix, key, valueDelimiter, jsonFile2[key], '\n');
-        return acc;
-      },
-      ['{\n'],
-    );
-    return `${diffArray.join('')}}`;
-  }
-  return 'Incorrect data';
+  const jsonFile1 = JSON.parse(readFileSync(fullPath1, 'utf8'));
+  const jsonFile2 = JSON.parse(readFileSync(fullPath2, 'utf8'));
+  const sortedKeys = _.sortBy(_.union(Object.keys(jsonFile1), Object.keys(jsonFile2)));
+  const firstFilePrefix = '  - ';
+  const secondFilePrefix = '  + ';
+  const bothFilesPrefix = '    ';
+  const valueDelimiter = ': ';
+
+  const diffArray = sortedKeys.reduce(
+    (acc, key) => {
+      if (Object.hasOwn(jsonFile1, key)) {
+        if (Object.hasOwn(jsonFile2, key)) {
+          if (jsonFile1[key] === jsonFile2[key]) {
+            acc.push(bothFilesPrefix, key, valueDelimiter, jsonFile1[key], '\n');
+          } else {
+            acc.push(firstFilePrefix, key, valueDelimiter, jsonFile1[key], '\n');
+            acc.push(secondFilePrefix, key, valueDelimiter, jsonFile2[key], '\n');
+          }
+        } else acc.push(firstFilePrefix, key, valueDelimiter, jsonFile1[key], '\n');
+      } else acc.push(secondFilePrefix, key, valueDelimiter, jsonFile2[key], '\n');
+      return acc;
+    },
+    ['{\n'],
+  );
+  return `${diffArray.join('')}}`;
 };
 
 const genDiff = (filepath1, filepath2, format) => {
