aa6faabfc4876cb738b079fb59eee58494bd14da
Allegro <alex.makovka@gmail.com>
2023-04-28 13:47:56 +0300

    edit index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,24 @@ const getFormat = (filepath1, filepath2) => {
   return 'incorrect';
 };
 
+const genDiffString = (key, value, withFile = 'bothFiles') => {
+  const firstFilePrefix = '  - ';
+  const secondFilePrefix = '  + ';
+  const bothFilesPrefix = '    ';
+  const valueDelimiter = ': ';
+
+  switch (withFile) {
+    case 'firstFile':
+      return `${firstFilePrefix}${key}${valueDelimiter}${value}`;
+    case 'secondFile':
+      return `${secondFilePrefix}${key}${valueDelimiter}${value}`;
+    case 'bothFiles':
+      return `${bothFilesPrefix}${key}${valueDelimiter}${value}`;
+    default:
+      return '';
+  }
+};
+
 const jsonDiff = (filepath1, filepath2) => {
   const fullPath1 = pathResolve(cwd(), filepath1);
   const fullPath2 = pathResolve(cwd(), filepath2);
@@ -19,23 +37,19 @@ const jsonDiff = (filepath1, filepath2) => {
   const jsonFile1 = JSON.parse(readFileSync(fullPath1, 'utf8'));
   const jsonFile2 = JSON.parse(readFileSync(fullPath2, 'utf8'));
   const sortedKeys = _.sortBy(_.union(Object.keys(jsonFile1), Object.keys(jsonFile2)));
-  const firstFilePrefix = '  - ';
-  const secondFilePrefix = '  + ';
-  const bothFilesPrefix = '    ';
-  const valueDelimiter = ': ';
 
   const diffArray = sortedKeys.reduce(
     (acc, key) => {
       if (Object.hasOwn(jsonFile1, key)) {
         if (Object.hasOwn(jsonFile2, key)) {
           if (jsonFile1[key] === jsonFile2[key]) {
-            acc.push(bothFilesPrefix, key, valueDelimiter, jsonFile1[key], '\n');
+            acc.push(genDiffString(key, jsonFile1[key], 'bothFiles'), '\n');
           } else {
-            acc.push(firstFilePrefix, key, valueDelimiter, jsonFile1[key], '\n');
-            acc.push(secondFilePrefix, key, valueDelimiter, jsonFile2[key], '\n');
+            acc.push(genDiffString(key, jsonFile1[key], 'firstFile'), '\n');
+            acc.push(genDiffString(key, jsonFile2[key], 'secondFile'), '\n');
           }
-        } else acc.push(firstFilePrefix, key, valueDelimiter, jsonFile1[key], '\n');
-      } else acc.push(secondFilePrefix, key, valueDelimiter, jsonFile2[key], '\n');
+        } else acc.push(genDiffString(key, jsonFile1[key], 'firstFile'), '\n');
+      } else acc.push(genDiffString(key, jsonFile2[key], 'secondFile'), '\n');
       return acc;
     },
     ['{\n'],
