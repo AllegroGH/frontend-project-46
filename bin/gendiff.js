22d6d2cd3e9fb8439725f5ef5ec06ef945380769
Allegro <alex.makovka@gmail.com>
2023-04-28 12:00:35 +0300

    add eslint, fix lint errors

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,14 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import { genDiff } from '../src/index.js';
+import genDiff from '../src/index.js';
 
-program.description(`Compares two configuration files and shows a difference.`);
+program.description('Compares two configuration files and shows a difference.');
 program.version('0.0.1', '-V, --version', 'output the version number');
 
 program.argument('<filepath1>');
 program.argument('<filepath2>');
 program.option('-f, --format <type>', 'output format');
 program.action((filepath1, filepath2, options) => {
-  /*if (options.format) {
-    console.log(options.format);
-  } else {
-    
-  }*/
   const diffString = genDiff(filepath1, filepath2, options.format);
   console.log(diffString);
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { cwd } from 'node:process';
-import { resolve as pathResolve } from 'node:path';
-import { extname } from 'node:path';
+import { resolve as pathResolve, extname } from 'node:path';
 import { existsSync, readFileSync } from 'node:fs';
 import _ from 'lodash';
 
@@ -9,12 +8,6 @@ const getFormat = (filepath1, filepath2) => {
   const format2 = extname(filepath2);
   if (format1 === format2) return format1;
   return 'incorrect';
-};
-
-export const genDiff = (filepath1, filepath2, format) => {
-  if (!format) {
-    if (getFormat(filepath1, filepath2) === '.json') return jsonDiff(filepath1, filepath2);
-  }
 };
 
 const jsonDiff = (filepath1, filepath2) => {
@@ -26,41 +19,57 @@ const jsonDiff = (filepath1, filepath2) => {
     const jsonFile2 = JSON.parse(readFileSync(fullPath2, 'utf8'));
     const keysFile1 = Object.keys(jsonFile1);
     const keysFile2 = Object.keys(jsonFile2);
+    const firstFilePrefix = '  - ';
+    const secondFilePrefix = '  + ';
+    const bothFilesPrefix = '    ';
 
-    const halfArr = keysFile1.reduce((acc, key) => {
+    const diffArr = keysFile1.reduce((acc, key) => {
       if (keysFile2.includes(key)) {
         if (jsonFile1[key] === jsonFile2[key]) {
-          acc.push(['    ', key, jsonFile1[key]]);
+          acc.push([bothFilesPrefix, key, jsonFile1[key]]);
         } else {
-          acc.push(['  - ', key, jsonFile1[key]]);
-          acc.push(['  + ', key, jsonFile2[key]]);
+          acc.push([firstFilePrefix, key, jsonFile1[key]]);
+          acc.push([secondFilePrefix, key, jsonFile2[key]]);
         }
-      } else acc.push(['  - ', key, jsonFile1[key]]);
+      } else acc.push([firstFilePrefix, key, jsonFile1[key]]);
       return acc;
     }, []);
 
-    const fullArr = keysFile2.reduce(
+    const fullDiffArr = keysFile2.reduce(
       (acc, key) => {
-        if (!keysFile1.includes(key)) acc.push(['  + ', key, jsonFile2[key]]);
+        if (!keysFile1.includes(key)) acc.push([secondFilePrefix, key, jsonFile2[key]]);
         return acc;
       },
-      [...halfArr]
+      [...diffArr],
     );
 
-    const sortedArr = _.sortBy(
-      _.sortBy(fullArr, (a) => a[0] !== '  - '),
-      1
+    const sortedDiffArr = _.sortBy(
+      _.sortBy(fullDiffArr, ([prefix]) => prefix !== '  - '),
+      ([, key]) => key,
     );
 
-    const result = sortedArr.reduce(
-      (acc, [prefix, key, value]) => {
-        acc.push(`${prefix}${key}: ${value}`);
-        acc.push('\n');
-        return acc;
-      },
-      ['{\n']
-    );
-    return `${result.join('')}}`;
+    const result = sortedDiffArr
+      .reduce(
+        (acc, [prefix, key, value]) => {
+          acc.push(`${prefix}${key}: ${value}`);
+          acc.push('\n');
+          return acc;
+        },
+        ['{\n'],
+      )
+      .join('');
+    return `${result}}`;
   }
   return 'Incorrect data';
 };
+
+const genDiff = (filepath1, filepath2, format) => {
+  if (!format) {
+    if (getFormat(filepath1, filepath2) === '.json') {
+      return jsonDiff(filepath1, filepath2);
+    }
+  }
+  return undefined;
+};
+
+export default genDiff;
